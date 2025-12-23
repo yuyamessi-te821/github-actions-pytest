b30065e578dc73e2ba7a231d1927bac5f54d18f2
yuya iino <yuyamessi.te821@gmail.com>
2025-12-23 14:46:44 +0900

    Update README

--- divide/test_divide.py
+++ divide/test_divide.py
@@ -2,7 +2,7 @@ import pytest
 from divide import divide
 
 def test_divide_normal():
-    assert divide(10, 2) == 5
+    assert divide(10, 5) == 2
 
 def test_divide_zero():
     with pytest.raises(ValueError):
